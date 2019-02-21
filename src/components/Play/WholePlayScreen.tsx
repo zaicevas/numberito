10a80fefe13678e603d7b86f26fbf5b2260bb9a4
Tomas <tozaicevas@gmail.com>
2019-02-21 11:31:28 -0800

    POC of native textinput (needs refactoring)

--- src/components/Play/WholePlayScreen.tsx
+++ src/components/Play/WholePlayScreen.tsx
@@ -1,14 +1,29 @@
 import * as React from "react";
-import { View, StyleSheet, ViewStyle } from "react-native";
+import { View, StyleSheet, ViewStyle, TextInput } from "react-native";
 import { Text } from "native-base";
 import Background from "../../utils/Background";
+import CalculatorButton from "./NumberButton";
 
 class WholePlayScreen extends React.Component {
+  state = {textInputText: "????"};
   render() {
     return (
       <View style={styles.container}>
         <Background />
-        <Text>yo</Text>
+        <TextInput
+        ref={input => { this.textInput = input }}
+          returnKeyType="done"
+          keyboardType="number-pad"
+          value={this.state.textInputText}
+                    placeholderTextColor="white"
+          caretHidden={true}
+          maxLength={4}
+          style={styles.textInput}
+          onChangeText={text => this.setState({textInputText: text})}
+          onFocus={() => { if (this.state.textInputText === "????") this.textInput.clear(); }}
+        />
+        <CalculatorButton operator="1"/>
+                <CalculatorButton operator="1"/>
       </View>
     );
   }
@@ -16,11 +31,21 @@ class WholePlayScreen extends React.Component {
 
 interface Style {
   container: ViewStyle;
+  textInput: ViewStyle;
 }
 const styles = StyleSheet.create<Style>({
   container: {
     flex: 1
-  }
+  },
+  textInput: {
+  height: 150,
+  fontSize: 42,
+  textAlign: 'center',
+  color: 'white',
+  borderColor: '#bfbfbf',
+  borderWidth: 1,
+  padding: 10
+},
 });
 
 export default WholePlayScreen;
