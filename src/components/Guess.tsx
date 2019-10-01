356c6f36bc576a893aa2dd9effc971700b205650
Tomas <tozaicevas@gmail.com>
2019-10-01 20:49:55 +0300

    code decomposed, bulls and cows colors swaped

---
+++ src/components/Guess.tsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import { SingleGuess } from '../types/index';
+import { View, Text, StyleSheet, TextStyle, ViewStyle } from 'react-native';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+import { Theme, Layout } from '../constants/index';
+import SvgBull from './SvgBull';
+
+interface GuessProps {
+  guess: SingleGuess;
+}
+
+const Guess: React.FC<GuessProps> = ({ guess }) => {
+  return (
+    <View style={styles.overlay}>
+      <View style={{ display: 'flex', flexDirection: 'row' }}>
+        <Text style={styles.text}>{guess.input}</Text>
+        <View style={{ display: 'flex', flexDirection: 'row', marginLeft: 4 }}>
+          <Text>{guess.bulls}</Text>
+          <SvgBull />
+          <Text>{guess.cows}</Text>
+          <MaterialCommunityIcons
+            name="cow"
+            color={Theme.colors.primary}
+            size={19}
+          />
+        </View>
+      </View>
+    </View>
+  );
+};
+
+interface Style {
+  text: TextStyle;
+  overlay: ViewStyle;
+}
+
+const styles = StyleSheet.create<Style>({
+  text: {
+    fontSize: 18,
+    color: 'black',
+  },
+  overlay: {
+    backgroundColor: Theme.colors.white,
+    borderRadius: Theme.sizes.radius,
+    padding: Layout.height * 0.007,
+    margin: Layout.width * 0.02,
+  },
+});
+
+export default Guess;
