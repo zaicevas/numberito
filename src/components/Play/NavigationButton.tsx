bcaaabb1dba10e4e47c06719d8f83ba9cd2203af
Tomas <tozaicevas@gmail.com>
2019-11-20 20:31:11 +0100

    more button animated

--- src/components/Play/NavigationButton.tsx
+++ src/components/Play/NavigationButton.tsx
@@ -10,7 +10,6 @@ import {
   Animated,
   Platform
 } from "react-native";
-import AnimatedButton from "./AnimatedButton";
 import { NavigationInjectedProps } from "react-navigation";
 import { MIDDLE_BUTTON_SIZE } from "../../constants/Navigation";
 import { InputState, SCREEN_PLAY } from "../../constants/Screens";
@@ -32,7 +31,7 @@ interface NavigationButtonProps extends NavigationInjectedProps {
 
 const ANIMATION_LENGTH = 1500;
 
-const SubButton: React.FC = ({ x, y, opacity, disabled }) => (
+const SubButton: React.FC = ({ x, y, opacity, disabled, children }) => (
   <Animated.View
     style={[
       {
@@ -99,30 +98,126 @@ const ChiliButton: React.FC = ({ navigation, untoggle }) => {
   );
 };
 
-const MoreButton: React.FC = ({ onPress, backgroundColor }) => {
-  return (
-    <AnimatedTouchable activeOpacity={0.7} onPress={() => onPress()}>
-      <Animated.View
-        style={[
-          {
-            left: 0,
-            alignItems: "center",
-            justifyContent: "center"
-          },
-          {
-            width: MIDDLE_BUTTON_SIZE,
-            height: MIDDLE_BUTTON_SIZE,
-            borderRadius: 100,
-            borderColor: "white",
-            backgroundColor
-          }
-        ]}
-      >
-        <Ionicons size={42} active name="md-more" color={Theme.colors.white} />
-      </Animated.View>
-    </AnimatedTouchable>
-  );
-};
+class MoreButton extends React.Component {
+  state = {
+    timeout: 0
+  };
+  private mode = new Animated.Value(0);
+
+  private firstX = this.mode.interpolate({
+    inputRange: [0, 1],
+    outputRange: [20, -70]
+  });
+  private firstY = this.mode.interpolate({
+    inputRange: [0, 1],
+    outputRange: [0, 60]
+  });
+  private secondX = this.mode.interpolate({
+    inputRange: [0, 1],
+    outputRange: [20, 0]
+  });
+  private secondY = this.mode.interpolate({
+    inputRange: [0, 1],
+    outputRange: [0, 90]
+  });
+  private thirdX = this.mode.interpolate({
+    inputRange: [0, 1],
+    outputRange: [20, 70]
+  });
+  private thirdY = this.mode.interpolate({
+    inputRange: [0, 1],
+    outputRange: [0, 60]
+  });
+  private opacity = this.mode.interpolate({
+    inputRange: [0, 1],
+    outputRange: [0, 1]
+  });
+  private rotation = this.mode.interpolate({
+    inputRange: [0, 1],
+    outputRange: ["0deg", "90deg"]
+  });
+
+  render() {
+    const { backgroundColor } = this.props;
+    return (
+      <>
+        {this.renderActions()}
+        <AnimatedTouchable
+          activeOpacity={0.7}
+          onPress={() => this.toggleView(false)}
+        >
+          <Animated.View
+            style={[
+              {
+                left: 0,
+                alignItems: "center",
+                justifyContent: "center",
+                transform: [{ rotate: this.rotation }]
+              },
+              {
+                width: MIDDLE_BUTTON_SIZE,
+                height: MIDDLE_BUTTON_SIZE,
+                borderRadius: 100,
+                borderColor: "white",
+                backgroundColor
+              }
+            ]}
+          >
+            <Ionicons
+              size={42}
+              active
+              name="md-more"
+              color={Theme.colors.white}
+            />
+          </Animated.View>
+        </AnimatedTouchable>
+      </>
+    );
+  }
+
+  public renderActions = () => {
+    return (
+      <View style={{ position: "absolute", bottom: 0 }}>
+        <SubButton x={this.firstX} y={this.firstY} opacity={this.opacity}>
+          <Ionicons
+            name={Platform.OS === "ios" ? "ios-refresh" : "md-refresh"}
+            size={16}
+            color={Theme.colors.white}
+          />
+        </SubButton>
+        <SubButton x={this.secondX} y={this.secondY} opacity={this.opacity}>
+          <MaterialCommunityIcons
+            name="numeric"
+            size={16}
+            color={Theme.colors.white}
+          />
+        </SubButton>
+        <SubButton x={this.thirdX} y={this.thirdY} opacity={this.opacity}>
+          <Entypo name="open-book" size={16} color={Theme.colors.white} />
+        </SubButton>
+      </View>
+    );
+  };
+
+  // should be rewritten without flag parameter
+  public toggleView = (automatic: boolean) => {
+    console.log("YE");
+    const { timeout } = this.state;
+    const { _value } = this.mode;
+    if (_value === 1 || !automatic) {
+      Animated.timing(this.mode, {
+        toValue: _value === 0 ? 1 : 0,
+        duration: 300
+      }).start();
+      clearTimeout(timeout);
+    }
+    if (_value === 0) {
+      const newTimeout = setTimeout(() => this.toggleView(true), AUTO_CLOSE);
+      this.setState({ timeout: newTimeout });
+    }
+  };
+}
+
 class MiddleButton extends React.Component {
   public state = {
     timeout: 0,
@@ -232,19 +327,11 @@ class MiddleButton extends React.Component {
         }}
       >
         {isFocused ? (
-          <View style={{ position: "absolute", bottom: 0 }}>
-            {this.renderActions()}
-          </View>
-        ) : null}
-        {!isFocused ? (
+          <MoreButton backgroundColor={activeTintColor} />
+        ) : (
           <ChiliButton
             navigation={navigation}
             untoggle={() => this.untoggleSubButtonsIfToggled()}
-          />
-        ) : (
-          <MoreButton
-            onPress={() => this.toggleView(false)}
-            backgroundColor={activeTintColor}
           />
         )}
       </View>
