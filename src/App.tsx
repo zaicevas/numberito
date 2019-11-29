28f23a1da6b62d4c1dba94e1cfe11a7cad6dc85d
Tomas <tozaicevas@gmail.com>
2019-11-29 16:54:58 +0100

    unused packages removed

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,14 @@
-import { light as lightTheme, mapping } from '@eva-design/eva';
 import React from 'react';
 import { Platform, StatusBar, StyleSheet, View } from 'react-native';
-import { ApplicationProvider } from 'react-native-ui-kitten';
 import AppContainer from './navigation/AppContainer';
 
 export default class App extends React.Component {
   public render() {
     return (
-      <ApplicationProvider mapping={mapping} theme={lightTheme}>
         <View style={styles.container}>
           {Platform.OS === 'ios' && <StatusBar barStyle="default" />}
           <AppContainer />
         </View>
-      </ApplicationProvider>
     );
   }
 }
