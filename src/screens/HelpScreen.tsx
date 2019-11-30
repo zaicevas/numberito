a7580b1a83b4ecb748c16d791a928b16b67b9c7d
Tomas <tozaicevas@gmail.com>
2019-11-30 20:51:04 +0100

    HelpScreen remade

--- src/screens/HelpScreen.tsx
+++ src/screens/HelpScreen.tsx
@@ -1,13 +1,36 @@
+import Constants from 'expo-constants';
 import React from 'react';
-import { ImageBackground, Text, View } from 'react-native';
-import { Layout, Theme } from '../constants/index';
+import { ScrollView } from 'react-native';
+import { Card, CardAction, CardButton, CardContent, CardImage, CardTitle } from 'react-native-material-cards';
+import { NavigationInjectedProps } from 'react-navigation';
+import { SCREEN_FEEDBACK } from '../constants/Screens';
 
-const HelpScreen: React.FC = () => {
-  return (<View>
-    <ImageBackground source={require('../../assets/awesome.jpg')} style={{ width: '100%', height: Layout.height * 0.25 }}>
-      <Text style={{ position: 'relative', color: Theme.colors.white, fontWeight: '800', top: Layout.height * 0.21, paddingLeft: 8 }}>What is this app?</Text>
-    </ImageBackground>
-  </View>);
+const HelpScreen: React.FC<NavigationInjectedProps> = ({ navigation }) => {
+  return (
+    <ScrollView contentContainerStyle={{ alignItems: 'center', marginBottom: Constants.statusBarHeight }}>
+        <Card style={{ paddingTop: Constants.statusBarHeight }}>
+  <CardImage
+    source={require('../../assets/awesome.jpg')}
+    resizeMode="center"
+  />
+  <CardTitle
+    title="Why?"
+   />
+  <CardContent text="Well, see it as a free time project made for people to enhance their
+  logical reasoning skills in a fun way. I highly appreciate every single suggestion, so please,
+  leave me a feedback and make my day! :)" />
+  <CardAction
+    separator={true}
+    inColumn={false}>
+    <CardButton
+      onPress={() => navigation.navigate(SCREEN_FEEDBACK)}
+      title="FEEDBACK"
+      color="blue"
+    />
+  </CardAction>
+</Card>
+      </ScrollView>
+  );
 };
 
 export default HelpScreen;
